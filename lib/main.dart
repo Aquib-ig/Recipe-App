e75ee0771ac468626949fe6558f226dbb61d9e4f
Aquib-ig <khanaquibak011@gmail.com>
2025-07-22 12:09:30 +0530

    add auth screens

--- lib/main.dart
+++ lib/main.dart
@@ -35,8 +35,6 @@ void main() async {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     final themeController = Provider.of<ThemeController>(context);
